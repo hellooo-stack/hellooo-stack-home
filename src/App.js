3e43f267a409c0b41229dcec6e56bdfecaa661cf
Jeb.Wang <jeb.wannng@gmail.com>
2022-08-06 15:01:49 +0800

    optimize: update home page description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    Hellooo!!! Here is the Home page for hellooo-stack projects! <br/>
+                    Hellooo!!! Here is the Home page for hellooo-stack organization projects! <br/>
                     We aim to build RELIABLE & FUNNY and USEFUL internet stuff,
                     you can touch me here at <a href="https://github.com/hellooo-stack">GitHub.com</a> .
                 </p>
